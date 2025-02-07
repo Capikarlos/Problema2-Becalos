a8afb6a6936d5ad8e967785cf7c6a130b3e5923b
Carlos Antonio López Prieto <lopezcarlosant@gmail.com>
2025-02-07 11:16:27 -0600

    Final

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Evaluador de Notas con Mensajes Personalizados
-
-// Asigna un valor a la variable nota. Puedes cambiar este valor para probar distintos casos.
-let nota = 85; // Ejemplo: 85. Prueba con 45, 60, 75, 0, etc.
+//Proyecto2
+//Carlos Antonio López Prieto
+// Asigno un valor a la variable nota.
+// Ejemplo: 85. Prueba con 45, 60, 75, 0, etc.
+let nota = 85; 
 let nota2 = 45;
 let nota3 = 75;
 let nota4 = 0;
@@ -9,7 +11,7 @@ let nota5 = 100;
 // Validamos que la nota sea un número entre 0 y 100
 function evaluarNota(nota){//Como crear una función en .c
     if (typeof nota === 'number' && nota >= 0 && nota <= 100) {
-        console.log(`La nota obtenida es: ${nota}`);
+        console.log(`La nota obtenida es: ${nota}`);//${nota} es una forma de concatenar
         
         if (nota >= 90){
             console.log("Excelente");
